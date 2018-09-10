1cfbc2c7c0eb49866a08f055038ad373893420b9
qing <eternity.zheng@gmail.com>
2018-09-10 11:18:59 +0800

    inti

---
+++ CoderRead/CoderRead/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  CoderRead
+//
+//  Created by qing on 2018/9/10.
+//  Copyright © 2018 qing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = UIColor.brown
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func showMsg(_ sender: UIButton) {
+    let alertController = UIAlertController(title: "Welcome to this book", message: "Hwllo", preferredStyle: UIAlertControllerStyle.alert)
+    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+    self.present(alertController, animated: true, completion:nil)
+  }
+  
+}
+
